fa648f1de2302d9000efb6409de4ee448ea76dca
Diego Cantelli <diegocantelli19@gmail.com>
2020-12-19 19:45:16 -0300

    Criação dos métodos de adição e remoção de stocks

--- src/app/components/manage/manage.component.ts
+++ src/app/components/manage/manage.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { StocksService } from '../../services/stocks.service';
+
 @Component({
   selector: 'app-manage',
   templateUrl: './manage.component.html',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageComponent implements OnInit {
 
-  constructor() { }
+  symbols: Array<string>;
+  stock: string;
+
+  constructor(private service: StocksService) {
+    this.symbols = service.get();
+  }
 
   ngOnInit() {
+  }
+
+  add() {
+    this.symbols = this.service.add(this.stock.toUpperCase());
+    this.stock = '';
+  }
+
+  remove(symbol) {
+    this.symbols = this.service.remove(symbol);
   }
 
 }
