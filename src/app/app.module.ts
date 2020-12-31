422f43fda95c2ffb5b91084d12d02f1a5796d7fb
Diego Cantelli <diegocantelli19@gmail.com>
2020-12-31 09:31:59 -0300

    Usando diretivas estruturais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ManageComponent } from './components/manage/manage.component';
 import { AppRoutes } from './app.routes';
 import { CardTypeDirective } from './directives/card-type.directive';
 import { CardHoverDirective } from './directives/card-hover.directive';
+import { DelayDirective } from './directives/delay.directive';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { CardHoverDirective } from './directives/card-hover.directive';
     DashboardComponent,
     ManageComponent,
     CardTypeDirective,
-    CardHoverDirective
+    CardHoverDirective,
+    DelayDirective
   ],
   imports: [
     BrowserModule,
